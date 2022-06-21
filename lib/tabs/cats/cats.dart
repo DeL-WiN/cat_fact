5b0fecf55df54379383c4bf4ea827a052ee144ec
weazzy <gamzatabdullaev@mail.ru>
2022-06-22 00:30:56 +0300

    factc

--- lib/tabs/cats/cats.dart
+++ lib/tabs/cats/cats.dart
@@ -4,9 +4,6 @@ import 'package:provider/provider.dart';
 import 'package:untitled3/tabs/cats/secon_cats.dart';
 
 import '../../blocs/photos_bloc/photos_bloc.dart';
-import '../../models/photos/photos.dart';
-import '../profile/profile.dart';
-import '../user_favorites/user_favorites.dart';
 
 class Cats extends StatefulWidget {
   const Cats({Key? key}) : super(key: key);
@@ -27,44 +24,46 @@ class _CatsState extends State<Cats> {
           return Scaffold(
             backgroundColor: Colors.black87,
             body: Center(
-              child: Container(
-                child: ListView.builder(
-                    itemCount: state.photos.length,
-                    itemBuilder: (context, i) {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        // crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const SecondCats()),
-                              );
-                            },
-                            child: Padding(
-                              padding: EdgeInsets.fromLTRB(12, 10, 12, 0),
-                              child: Image.network(
-                                state.photos[i].url,
-                              ),
+              child: ListView.builder(
+                  itemCount: state.photos.length,
+                  itemBuilder: (context, i) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SecondCats(
+                                        fact: state.factss!.fact,
+                                        catImageUrl: state.photos[i].url,
+                                      )),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 10, 12, 0),
+                            child: Image.network(
+                              state.photos[i].url,
                             ),
                           ),
-                          IconButton(
-                              onPressed: () => context.read<PhotosBloc>().ret(state.photos[i]) ,
-                              icon: Icon(
-                                Icons.favorite_border_sharp,
-                                color: Colors.red,
-                              )),
-                        ],
-                      );
-                    }),
-              ),
+                        ),
+                        IconButton(
+                            onPressed: () =>
+                                context.read<PhotosBloc>().ret(state.photos[i]),
+                            icon: const Icon(
+                              Icons.favorite_border_sharp,
+                              color: Colors.red,
+                            )),
+                      ],
+                    );
+                  }),
             ),
           );
         }
-        return Center(
+        return const Center(
           child: CircularProgressIndicator(),
         );
       }),
--- lib/tabs/cats/secon_cats.dart
+++ lib/tabs/cats/secon_cats.dart
@@ -1,38 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:untitled3/blocs/photos_bloc/photos_bloc.dart';
 
 class SecondCats extends StatelessWidget {
-  const SecondCats({Key? key}) : super(key: key);
+  final String fact;
+  final String catImageUrl;
+
+  const SecondCats({Key? key, required this.fact, required this.catImageUrl})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: BlocBuilder<PhotosBloc, PhotosState>(
-      builder: (context, state) {
-        if (state is PhotosStateLoading) {
-          BlocProvider.of<PhotosBloc>(context).add(LoadingDataEvent());
-        }
-        if (state is PhotosStateLoaded) {
-          return Scaffold(
-            backgroundColor: Colors.black87,
-            body: Column(
-              children: [
-                Image.network(state.photos[0].url),
-                Text(
-                  state.factss!.fact,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ],
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: Column(
+        children: [
+          Image.network(catImageUrl),
+          Text(
+            fact,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
             ),
-          );
-        }
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    ));
+          ),
+        ],
+      ),
+    );
   }
 }
